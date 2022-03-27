6d98795fa30618141289ec918de4d5f30f70ab50
eu860106 <eu860106@gmail.com>
2022-03-28 01:15:30 +0800

    1.0

--- anigamer app/Form1.cs
+++ anigamer app/Form1.cs
@@ -25,10 +25,7 @@ namespace anigamer_app
 
         private void Web_Search_Button1_Click(object sender, EventArgs e)
         {
-            Output_Box.Text = "";
-            Output_Box.Links.Clear();
-
-            if(web_input.Text.IndexOf("https://forum.gamer.com.tw/B.php?") == -1)
+            if (web_input.Text.IndexOf("https://forum.gamer.com.tw/B.php?") == -1)
             {
                 Output_Box.Text += "輸入網址錯誤\n";
                 return;
@@ -36,57 +33,26 @@ namespace anigamer_app
             }
 
             int jump_page;
-            if(page_box.Text != "")
+            if (page_box.Text != "")
             {
-                if(int.TryParse(page_box.Text, out jump_page) != true)
+                if (int.TryParse(page_box.Text, out jump_page) != true)
                 {
                     Output_Box.Text += "輸入頁碼錯誤\n";
                     return;
                     return;
                 }
 
-                if(jump_page < 1)
+                if (jump_page < 1)
                 {
                     Output_Box.Text += "輸入頁碼錯誤\n";
                     return;
                     return;
                 }
 
-                driver.Navigate().GoToUrl(web_input.Text + "&page=" + jump_page);
                 page_num = jump_page;
             }
-            else
-            {
-                driver.Navigate().GoToUrl(web_input.Text);
-            }
-
-            timeouts = driver.Manage().Timeouts();
-            timeouts.ImplicitWait = new TimeSpan(0, 0, 10);
-
-            Output_Box.Text += driver.Title + "\n";
-            Output_Box.Links.Add(0, driver.Title.Length, web_input.Text);
-            int output_number = 0;
             
-            
-            Output_Box.Text += "\nPage " + page_num + " :\n";
-
-            var titles = driver.FindElements(By.ClassName("b-list__main__title"));
-
-            for (int i = 0; i < titles.Count; i++)
-            {
-                if(titles[i].Text.IndexOf(title_condition.Text) != -1)
-                {
-                    if(titles[i].Text.IndexOf(title_keyword.Text) != -1)
-                    {
-                        var titles_website = titles[i].GetAttribute("href");
-                        Output_Box.Text += titles[i].Text + "\n";
-                        Output_Box.Links.Add(Output_Box.Text.Length - titles[i].Text.Length, titles[i].Text.Length, titles_website);
-                        output_number++;
-                    }
-                }
-            }
-
-            Output_Box.Text += "搜尋結果 : 共 " + output_number + " 項\n";
+            webload(page_num);
         }
 
         private void Output_Box_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
@@ -116,34 +82,7 @@ namespace anigamer_app
         private void next_page_Click(object sender, EventArgs e)
         {
             page_num++;
-            driver.Navigate().GoToUrl(web_input.Text + "&page=" + page_num);
-            timeouts.ImplicitWait = new TimeSpan(0, 0, 5);
-
-            Output_Box.Text = "";
-            Output_Box.Links.Clear();
-            int output_number = 0;
-
-            Output_Box.Text += driver.Title + "\n";
-            Output_Box.Links.Add(0, driver.Title.Length, web_input.Text);
-            Output_Box.Text += "\nPage " + page_num + " :\n";
-
-            var titles = driver.FindElements(By.ClassName("b-list__main__title"));
-
-            for (int i = 0; i < titles.Count; i++)
-            {
-                if (titles[i].Text.IndexOf(title_condition.Text) != -1)
-                {
-                    if (titles[i].Text.IndexOf(title_keyword.Text) != -1)
-                    {
-                        var titles_website = titles[i].GetAttribute("href");
-                        Output_Box.Text += titles[i].Text + "\n";
-                        Output_Box.Links.Add(Output_Box.Text.Length - titles[i].Text.Length, titles[i].Text.Length, titles_website);
-                        output_number++;
-                    }
-                }
-            }
-
-            Output_Box.Text += "搜尋結果 : 共 " + output_number + " 項\n";
+            webload(page_num);
         }
 
         private void pre_page_button_Click(object sender, EventArgs e)
@@ -154,7 +93,13 @@ namespace anigamer_app
                 page_num = 1;
             }
 
+            webload(page_num);
+        }
+        
+        private void webload(int page_num)
+        {
             driver.Navigate().GoToUrl(web_input.Text + "&page=" + page_num);
+            timeouts = driver.Manage().Timeouts();
             timeouts.ImplicitWait = new TimeSpan(0, 0, 5);
 
             Output_Box.Text = "";
@@ -180,10 +125,10 @@ namespace anigamer_app
                     }
                 }
             }
-            
+
             Output_Box.Text += "搜尋結果 : 共 " + output_number + " 項\n";
         }
-        
+
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
             driver.Quit();
